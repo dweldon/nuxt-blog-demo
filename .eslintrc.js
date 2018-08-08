a66aa5e6d2e863ef921895bba7e2ced25bd8b404
David Weldon <dweldon@gmail.com>
2018-08-07 22:47:25 -0700

    Initialize project

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'airbnb-base',
+    'plugin:vue/recommended',
+  ],
+  rules: {
+    'no-console': 'warn',
+    'no-restricted-syntax': 'off',
+  },
+};
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template lang="html">
+  <h1 class="header">Hello World!</h1>
+</template>
+
+<style lang="stylus" scoped>
+.header
+  color: darkred
+  text-align: center
+  margin: 1em
+</style>
