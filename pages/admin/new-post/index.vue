f4fb3f86230cdef2faf7b5ae406c7905b6d84807
David Weldon <dweldon@gmail.com>
2018-08-08 16:11:27 -0700

    Add form for new posts

---
+++ pages/admin/new-post/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="admin-new-post-page">
+    <section class="new-post-form">
+      <form @submit.prevent="onSave">
+        <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
+
+        <AppControlInput v-model="editedPost.title">Title</AppControlInput>
+
+        <AppControlInput v-model="editedPost.thumbnailUrl">Thumbnail URL</AppControlInput>
+
+        <AppControlInput
+          v-model="editedPost.content"
+          control-type="textarea"
+        >Content</AppControlInput>
+
+        <AppButton type="submit">Save</AppButton>
+
+        <AppButton
+          type="button"
+          style="margin-left: 10px"
+          btn-style="cancel"
+          @click="onCancel"
+        >Cancel</AppButton>
+      </form>
+    </section>
+  </div>
+</template>
+
+<script>
+import AppButton from '~/components/UI/AppButton';
+import AppControlInput from '~/components/UI/AppControlInput';
+
+export default {
+  components: { AppButton, AppControlInput },
+  data() {
+    return {
+      editedPost: {
+        title: '',
+        author: '',
+        content: '',
+        thumbnailUrl: '',
+      },
+    };
+  },
+  methods: {
+    onSave() {
+      console.log(this.editedPost);
+    },
+    onCancel() {
+      this.$router.push('/admin');
+    },
+  },
+};
+</script>
+
+<style lang="css">
+</style>
