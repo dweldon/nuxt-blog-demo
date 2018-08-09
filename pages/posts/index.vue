8e7d1611478e185ac06199b6fdb680d851d49331
David Weldon <dweldon@gmail.com>
2018-08-09 16:09:16 -0700

    Load posts from the store

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -11,26 +11,27 @@ const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 export default {
   components: { PostList },
-  async asyncData() {
+  async fetch({ store }) {
     await snooze(1000);
-    return {
-      loadedPosts: [{
-        id: '1',
-        title: 'What is your puppy thinking about?',
-        isAdmin: 'isAdmin',
-        previewText: 'Your canine companion slumbers by your side, but is she dreaming of you?',
-        thumbnailUrl: 'https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg',
-      }, {
-        id: '2',
-        title: 'Are zebras just horses with stripes?',
-        isAdmin: 'isAdmin',
-        previewText: 'Zebras, horses, and donkeys have differing numbers of chromosomes, in fact...',
-        thumbnailUrl: 'https://images.pexels.com/photos/39245/zebra-stripes-black-and-white-zoo-39245.jpeg',
-      }],
-    };
+    const loadedPosts = [{
+      id: '1',
+      title: 'What is your puppy thinking about?',
+      isAdmin: 'isAdmin',
+      previewText: 'Your canine companion slumbers by your side, but is she dreaming of you?',
+      thumbnailUrl: 'https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg',
+    }, {
+      id: '2',
+      title: 'Are zebras just horses with stripes?',
+      isAdmin: 'isAdmin',
+      previewText: 'Zebras, horses, and donkeys have differing numbers of chromosomes, in fact...',
+      thumbnailUrl: 'https://images.pexels.com/photos/39245/zebra-stripes-black-and-white-zoo-39245.jpeg',
+    }];
+    store.commit('setPosts', loadedPosts);
   },
-  created() {
-    this.$store.dispatch('setPosts', this.loadedPosts);
+  computed: {
+    loadedPosts() {
+      return this.$store.getters.loadedPosts;
+    },
   },
 };
 </script>
